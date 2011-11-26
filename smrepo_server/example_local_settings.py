23daa41b6f114d51934df4c0439669b24c2a3fc5
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2011-11-25 17:16:32 -0800

    Django settings setup

---
+++ smrepo_server/example_local_settings.py
@@ -0,0 +1,17 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'plugins.db.sq3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+SECRET_KEY = ''
+
